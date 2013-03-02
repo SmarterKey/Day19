9bc73ed7f3eb4bbdc36b46a62251dd74477f68b3
David SK app developer <david@smarterkey.com>
2013-03-02 11:23:49 +0100

    Initial Commit

---
+++ DayNineteen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DayNineteen
+//
+//  Created by Administrator on 3/2/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
